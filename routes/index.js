76f5b751e1ea781b33b768000b547c5ef0a5dec2
Camilo Lavado <camilolavado.it@gmail.com>
2026-05-08 00:30:35 -0400

    fix error rutas

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+
 const usuariosRoutes = require('./usuarios');
 const candidatosRoutes = require('./candidatos');
 const cargosRoutes = require('./cargos');
@@ -22,7 +23,7 @@ router.use('/entrevistadores', entrevistadoresRoutes);
 router.use('/entrevistas', entrevistasRoutes);
 router.use('/experiencias', experienciasRoutes);
 
-router.use('*', (req, res) => {
+router.use((req, res) => {
     res.status(404).json({ error: 'Ruta no encontrada en la API' });
 });
 
